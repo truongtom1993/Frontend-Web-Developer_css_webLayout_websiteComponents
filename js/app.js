688611ff9f8c60d4bf1e71a350fddb132032d758
TruongDN <dang_nhat_truong@hotmail.com>
2023-02-28 22:01:41 +0700

    done project landing page

--- js/app.js
+++ js/app.js
@@ -1,100 +1,87 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+const body = document.body;
+let preValue = window.pageYOffset;
 
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
 function getSectionList() {
 	const sectionList = $$('section[id^=section]')
 	if (sectionList?.length > 0) return sectionList;
 	return
 }
+function throttle(callback, limit) {
+	let waiting = false;
+	return function () {
+		if (!waiting) {
+			callback.apply(this, arguments);
+			waiting = true;
+			setTimeout(function () {
+				waiting = false;
+			}, limit);
+		}
+	};
+}
 
+function makeActive(sections, aTagList) {
+	const value = 471;
+	for (let index = 0; index < sections.length; index++) {
+		const box = sections[index].getBoundingClientRect();
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
+		if (box.top <= value && box.bottom >= value) {
+			sections[index].classList.add('active')
+			aTagList[index].classList.add('active')
+		} else {
+			sections[index].classList.remove('active')
+			aTagList[index].classList.remove('active')
 
-const createNavBar = () => {
-	const sectionList = getSectionList();
-	const ulNavbar = $("#_2feeb3");
-	if (ulNavbar) {
-		for (let index = 0; index < sectionList.length; index++) {
-			const section = sectionList[index];
-			const sectionName = section.dataset?.nav || `Section ${Math.random().toFixed(3)}`;
-
-			const li = document.createElement('li');
-			li.classList.add('navbar_item')
-			li.innerHTML = `<a class="menu__link active">${sectionName}</a>`;
-			li.onclick = () => {
-				section.scrollIntoView({ behavior: 'smooth' });
-				for (let index = 0; index < sectionList.length; index++) {
-					sectionList[index].classList.remove('active')
-				}
-				section.classList.add('active');
-			}
-			ulNavbar.insertAdjacentElement('beforeend', li)
 		}
 	}
 }
 
 
+const header = $('#_922ba6')
+const nav = document.createElement(`nav`);
+nav.classList.add('navbar__menu')
+const ulNavbar = document.createElement(`ul`);
+ulNavbar.classList.add('navbar__list')
+header.appendChild(nav)
+nav.appendChild(ulNavbar)
+const sectionList = getSectionList();
+const aTagList = [];
+for (let index = 0; index < sectionList.length; index++) {
+	const section = sectionList[index];
+	const sectionName = section.dataset?.nav || `Section ${Math.random().toFixed(3)}`;
+	const liTag = document.createElement('li');
+	liTag.classList.add('navbar_item')
+	const aTag = document.createElement(`a`);
+	aTag.classList.add('menu__link')
+	if (index === 0) aTag.classList.add('active')
+	aTag.innerText = sectionName
+	liTag.appendChild(aTag)
+	liTag.onclick = (event) => {
+		event.preventDefault();
+		section.scrollIntoView({ behavior: 'smooth' });
+		for (let index = 0; index < sectionList.length; index++) {
+			sectionList[index].classList.remove('active')
+			aTagList[index].classList.remove('active')
+		}
+		event.target.classList.add('active')
+		section.classList.add('active');
+	}
+	aTagList.push(aTag)
+	ulNavbar.insertAdjacentElement('beforeend', liTag)
 
-// build the nav
-createNavBar()
-
-// Add class 'active' to section when near top of viewport
-
-const body = document.body;
-const eventBody = (event) => {
-	console.info(`🎁 js/app.js	Line:81	ID:70b7ff`,);
-	
 }
+
+const eventBody = throttle((event) => {
+	makeActive(sectionList, aTagList);
+	const currentValue = window.pageYOffset
+	if (preValue >= currentValue) {
+		header.classList.remove('minify')
+	} else {
+		header.classList.add('minify')
+	}
+	preValue = currentValue
+}, 20)
+
 body.addEventListener('wheel', eventBody)
-console.info(`🎁 js/app.js	Line:80	ID:b138b2`, body);
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
