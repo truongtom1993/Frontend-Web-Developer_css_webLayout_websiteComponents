0fa5036ba028000cd7ef81a4da050f6ac0eb262d
TruongTom <truongtom1993@gmail.com>
2023-02-28 17:26:53 +0700

    dang lam do task

--- js/app.js
+++ js/app.js
@@ -33,20 +33,56 @@ const $$ = document.querySelectorAll.bind(document);
  * Start Helper Functions
  * 
 */
-
+function getSectionList() {
+	const sectionList = $$('section[id^=section]')
+	if (sectionList?.length > 0) return sectionList;
+	return
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
-// build the nav
+const createNavBar = () => {
+	const sectionList = getSectionList();
+	const ulNavbar = $("#_2feeb3");
+	if (ulNavbar) {
+		for (let index = 0; index < sectionList.length; index++) {
+			const section = sectionList[index];
+			const sectionName = section.dataset?.nav || `Section ${Math.random().toFixed(3)}`;
 
+			const li = document.createElement('li');
+			li.classList.add('navbar_item')
+			li.innerHTML = `<a class="menu__link active">${sectionName}</a>`;
+			li.onclick = () => {
+				section.scrollIntoView({ behavior: 'smooth' });
+				for (let index = 0; index < sectionList.length; index++) {
+					sectionList[index].classList.remove('active')
+				}
+				section.classList.add('active');
+			}
+			ulNavbar.insertAdjacentElement('beforeend', li)
+		}
+	}
+}
+
+
+
+// build the nav
+createNavBar()
 
 // Add class 'active' to section when near top of viewport
 
+const body = document.body;
+const eventBody = (event) => {
+	console.info(`🎁 js/app.js	Line:81	ID:70b7ff`,event.);
+	
+}
+body.addEventListener('wheel', eventBody)
+console.info(`🎁 js/app.js	Line:80	ID:b138b2`, body);
 
 // Scroll to anchor ID using scrollTO event
 
@@ -54,7 +90,7 @@ const $$ = document.querySelectorAll.bind(document);
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
