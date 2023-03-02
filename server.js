560e4e2c6483d8fb804446a3927464792c53e0cf
TruongDN <dang_nhat_truong@hotmail.com>
2023-03-02 19:53:12 +0700

    dang css chua xong

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 // Setup empty JS object to act as endpoint for all routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = 1300;
+
 projectData = {};
 
 // Require Express to run server and routes
@@ -13,7 +19,13 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
 
+app.use('/website', express.static('website'));
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
+app.listen(PORT, () => {
+	console.info(`🎁 server.js	Line:22	ID:af4e61`);
+});
 // Setup Server
--- website/app.js
+++ website/app.js
@@ -1,5 +1,14 @@
 /* Global Variables */
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const generateBtn = $('#generate')
+generateBtn.addEventListener(`click`, () => {
+	console.info(`🎁 website/app.js	Line:12	ID:39cd24`,);
+	
+})
