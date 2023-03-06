a6a394c7217e67035b5f07e4115aa28089c5f06d
TruongTom <truongtom1993@gmail.com>
2023-03-06 17:13:44 +0700

    lam do huong dan p2

--- src/server/index.js
+++ src/server/index.js
@@ -4,20 +4,25 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+const dotenv = require('dotenv');
+dotenv.config();
+console.info(`🎁 src/server/index.js	Line:9	ID:6a2e0c`,process.env.API_KEY);
+
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.listen(8080, function () {
+	console.log('Example app listening on port 8080!')
 })
